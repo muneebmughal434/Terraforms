9ceff649a6f54c036e76c8264d946a83dcf20569
Muhammad Muneeb <85062890+muneebmughal434@users.noreply.github.com>
2025-08-12 20:38:31 -0400

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  value       = aws_s3_bucket.site.bucket
-  description = "Name of the S3 bucket"
-}
-
-output "website_endpoint" {
-  value       = aws_s3_bucket_website_configuration.site.website_endpoint
-  description = "S3 static website URL"
-}
